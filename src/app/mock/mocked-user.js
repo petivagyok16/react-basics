1a87c89364d134c0ea05727790124d5a050dbe46
Peter Palocz <peter.palocz91@gmail.com>
2017-04-17 15:10:29 +0200

    feat(mocked-user): add mocked users

---
+++ src/app/mock/mocked-user.js
@@ -0,0 +1,24 @@
+export const users = 
+[
+  {
+    id: 1,
+    name: "Kata",
+    age: "24",
+    hobbies: [{ id: 1, value: "Tennis" }, { id: 2, value: "Running" }, { id: 3, value: "Yoga" }],
+    available: true,
+  },
+  {
+    id: 2,
+    name: "Peter",
+    age: "25",
+    hobbies: [{ id: 1, value: "Running" }, { id: 2, value: "Mountain-biking" }, { id: 3, value: "CS:GO" }],
+    available: false,
+  },
+  {
+    id: 3,
+    name: "Matthias",
+    age: "25",
+    hobbies: [{ id: 1, value: "CS:GO" }],
+    available: true,
+  }
+];
