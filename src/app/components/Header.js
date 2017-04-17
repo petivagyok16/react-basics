c63b9568191f88b86708f7b36056f2e31a9294c0
Peter Palocz <peter.palocz91@gmail.com>
2017-04-17 20:54:51 +0200

    refactor(Header): refactor to stateless component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-default">
-        <div className="container">
-          <div className="navbar-header">
-            <ul className="nav navbar-nav">
-              <li><a href="#">Home</a></li>
-            </ul>
-          </div>
+// Stateless component. State change is impossible for this component so that its a constant not a class.
+export const Header = (props) => {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <ul className="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+          </ul>
         </div>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  )
 }
