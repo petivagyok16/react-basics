f4776334e057729854c841fd0cd189152a2b0d49
Peter Palocz <peter.palocz91@gmail.com>
2017-04-17 15:10:01 +0200

    feat(Home): add dynamic cmp rendering

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { User } from "./User"
+import * as mockedUsers from "../mock/mocked-user";
+
 export class Home extends React.Component {
+  users = mockedUsers.users;
+
   render() {
     return (
       <div>
-        <h1>Hi. I'm react. I'm alive in Home component.</h1>
+        <h1>Hi. I'm react. I'm alive.</h1>
+        {
+          this.users.map( (user) => <User key={ user.id } user={ user }/> )
+        }
       </div>
     );
   }
