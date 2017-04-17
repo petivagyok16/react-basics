922160f9a97884b8d00bb625decea299acfdb24f
Peter Palocz <peter.palocz91@gmail.com>
2017-04-17 12:45:06 +0200

    feat(Home): add home component

---
+++ src/app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi. I'm react. I'm alive in Home component.</h1>
+      </div>
+    );
+  }
+}
