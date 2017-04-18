4d1f39ca783f3e544e84f02f4b92211b9dfc1b80
Peter Palocz <peter.palocz91@gmail.com>
2017-04-18 10:19:43 +0200

    feat(User): add communication between parent & child

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -13,6 +13,10 @@ export class User extends React.Component {
     });
   }
 
+  changeGreeter() {
+    this.props.changeGreeter(this.state.name); // This triggers parent component's onChangeGreeter method with the params from the child.
+  }
+
   render() {
     return (
       <div>
@@ -22,11 +26,12 @@ export class User extends React.Component {
           </div>
           <div className="panel-footer">
             { this.state.age } years old.<br/>
-            Hobbies: { 
-              this.state.hobbies.map( (hobby) => 
+            Hobbies: {
+              this.state.hobbies.map( (hobby) =>
                 <span key={ hobby.id }>{ hobby.value }{ this.state.hobbies[this.state.hobbies.length-1] === hobby ? null : ", " }</span>)
             }
             <button className="pull-right" onClick={ this.makeOlder.bind(this) /* () => this.makeOlder() */ } >Make me older!</button>
+            <button className="pull-right" onClick={ this.changeGreeter.bind(this) }>Greet!</button>
           </div>
         </div>
       </div>
@@ -37,8 +42,8 @@ export class User extends React.Component {
  * WARNING THIS ISDEPRECATED. Use prop-types npm package instead.
 /*
 User.propTypes = {
-  id: React.PropTypes.number, 
-  name: React.PropTypes.string, 
-  age: React.PropTypes.string, 
+  id: React.PropTypes.number,
+  name: React.PropTypes.string,
+  age: React.PropTypes.string,
   hobbies: React.PropTypes.object,
 }*/
