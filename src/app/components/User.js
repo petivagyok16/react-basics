2e6b98ea19962a4f9974a954e73cae30e8db49ff
Peter Palocz <peter.palocz91@gmail.com>
2017-04-22 10:28:16 +0200

    feat(User): add component lifecycles

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -4,7 +4,7 @@ export class User extends React.Component {
 
   constructor(props) {
     super();
-    this.state = {...props.user}; // destructure user object into state object
+    this.state = { ...props.user, mounted: true }; // destructure user object into state object
   }
 
   makeOlder() {
@@ -23,15 +23,29 @@ export class User extends React.Component {
     });
   }
 
+  onHeaderChange() {
+    this.setState({
+      mounted: !this.state.mounted
+    })
+  }
+
   render() {
+    /**
+     * We can store whole html blocks in a variable and than using that after in render()
+     */
+    let userHeader = '';
+    if (this.state.mounted) {
+      userHeader = (
+        <div className="panel-body">
+          { this.state.name }
+          <input className="pull-right" type="text" value={ this.state.name } onChange={ (event) => this.onNameChange(event) }/>
+          {/*<button className="pull-right">Change</button>*/}
+        </div> )
+    }
     return (
       <div>
         <div className="panel panel-success">
-          <div className="panel-body">
-            { this.state.name }
-            <input className="pull-right" type="text" value={ this.state.name } onChange={ (event) => this.onNameChange(event) }/>
-            <button className="pull-right">Change</button>
-          </div>
+          { userHeader }
           <div className="panel-footer">
             { this.state.age } years old.<br/>
             Hobbies: {
@@ -40,10 +54,43 @@ export class User extends React.Component {
             }
             <button className="pull-right" onClick={ this.makeOlder.bind(this) /* () => this.makeOlder() */ } >Make me older!</button>
             <button className="pull-right" onClick={ this.changeGreeter.bind(this) }>Greet!</button>
+            <button className="pull-right" onClick={ this.onHeaderChange.bind(this) }>(Un)mount header</button>
           </div>
         </div>
       </div>
     );
+  }
+
+  // Component Lifecycles:
+
+  componentWillMount() {
+    console.info(`[UserCmp][WillMount]`);
+  }
+
+  componentDidMount() {
+    console.info(`[UserCmp][DidMount]`);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.info(`[UserCmp][WillRecieveProps]`, nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.info(`[UserCmp][ShouldUpdate]`);
+    return true;
+    // return false if update is not necessary
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.info(`[UserCmp][WillUpdate]`);
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    console.info(`[UserCmp][DidUpdate]`, nextState);
+  }
+
+  componentWillUnmount() {
+    console.info(`[UserCmp][WillUnmount]`);
   }
 }
 /**
