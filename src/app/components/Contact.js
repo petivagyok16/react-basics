f3f4b37266625194a09d5c767f62e9570750bf0a
Peter Palocz <peter.palocz91@gmail.com>
2017-04-24 10:02:20 +0200

    feat(Contact): add contact page

---
+++ src/app/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from "react-dom";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Contact Page</h3>
+        <p>Contact page params: </p>
+      </div>
+    )
+  }
+}
